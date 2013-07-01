41546f3b916d0fdb83efd019ed54e64feebcff80
Anil Kumar Reddy Gaddam <ganilreddy@hotmail.com>
2013-07-02 00:41:23 +0530

    Create shopping_cart_page.rb

---
+++ RSpec-Watir/lib/pages/shopping_cart_page.rb
@@ -0,0 +1,7 @@
+class ShoppingCartPage
+  include PageObject
+
+  button(:proceed_to_checkout, :value => 'Complete the Adoption')
+  button(:continue_shopping, :value => 'Adopt Another Puppy')
+
+end
