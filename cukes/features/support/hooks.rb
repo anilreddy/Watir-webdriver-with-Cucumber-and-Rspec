ac2aec1e8810231b13b7e2fd29c741c4a934996c
Anil Kumar Reddy Gaddam <ganilreddy@hotmail.com>
2014-05-24 12:50:12 +0530

    added Headless browsers usage in page-object

--- cukes/features/support/hooks.rb
+++ cukes/features/support/hooks.rb
@@ -1,14 +1,27 @@
 require 'watir-webdriver'
 
-
-
 Before do
+  
+  # For Phantom JS use
+  # @browser = Watir::Browser.new :phantomjs('') #(location of the phantomjs)
+  
+  # For HTML Unit Driver use
+  # Before using HTML Unit Driver. Start Selenium Server
+  
+  # You can use start_server.rb script in the support for starting the server
+  
+  # capabilities = WebDriver::Remote::Capabilities.htmlunit(:javascript_enabled => true)
+  # @browser = Watir::Browser.new(:remote, :url => 'http://127.0.0.1:4444/wd/hub', :desired_capabilities => capabilities)
+  
   @browser = Watir::Browser.new :firefox
   @browser.window.maximize
 end
 
 
 After do |scenario|
+  
+  # Note: For HTML Unit Driver comment out the screenshot script
+  
   if scenario.failed?
     Dir::mkdir('screenshots') if not File.directory?('screenshots')
     screenshot = "./screenshots/FAILED_#{scenario.name.gsub(' ','_').gsub(/[^0-9A-Za-z_]/, '')}.png"
